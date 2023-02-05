a52fe6ea233c6de0e8f40410fccf9565d64af3ed
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-02-05 13:51:18 +0100

    Added baseline package setup

---
+++ Packages/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let globalDependencies: [Package.Dependency] = [
+  
+]
+
+let platforms: [SupportedPlatform] = [
+  .iOS(.v15),
+  .macOS(.v13)
+]
+
+let package = Package(
+  name: "Packages",
+  platforms: platforms,
+  products: [
+    .library(
+      name: "Packages",
+      targets: ["Packages"]
+    ),
+  ],
+  dependencies: globalDependencies,
+  targets: [
+    .target(
+      name: "Packages",
+      dependencies: []
+    ),
+  ]
+)
