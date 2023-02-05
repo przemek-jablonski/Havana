9146955f1ae37f657829ba73e79c8b2835168937
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-02-05 02:07:57 +0100

    Initial Commit

---
+++ Havana/HavanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavanaApp.swift
+//  Havana
+//
+//  Created by Przemyslaw Jablonski on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HavanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
