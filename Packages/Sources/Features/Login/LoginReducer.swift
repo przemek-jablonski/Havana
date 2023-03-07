cad07f6a5e9f62b3f498b8bcf969037651218f1a
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-03-08 00:33:50 +0100

    Basic action / state routing

--- Packages/Sources/Features/Login/LoginReducer.swift
+++ Packages/Sources/Features/Login/LoginReducer.swift
@@ -2,30 +2,36 @@ import ComposableArchitecture
 import Foundation
 
 public struct LoginReducer: ReducerProtocol {
-  public enum State: Equatable {
+  public enum State: String, Equatable {
     /**
      Initial state of the screen - presenting greeting to the user. Here user can select which login flow to use.
      */
     case greeting
+    
     /**
-     Displaying editable fields where user can input PAT (Private Access Token)
+     Displaying editable fields where user can input Private Access Token.
      */
-    case privateAccessTokenFlowSelected
+    case privateAccessTokenLoginFlow
+    
     /**
-     
+      Displaying webview with the oAuth2 login.
      */
-    case oauthFlowSelected
+    case oauthLoginFlow
   }
   
   public enum Action {
-    case userDidTapLoginWithPrivateAccessTokenButton(token: String)
-    case userDidTapLoginWithGithubButton(token: String)
+    case userDidTapLoginWithPrivateAccessTokenButton
+    case userDidTapLoginWithGithubButton
   }
   
   public var body: some ReducerProtocol<State, Action> {
     Reduce { state, action in
       switch action {
-        case .userDidTapLoginButton(let token):
+        case .userDidTapLoginWithGithubButton:
+          state = .oauthLoginFlow
+          return .none
+        case .userDidTapLoginWithPrivateAccessTokenButton:
+          state = .privateAccessTokenLoginFlow
           return .none
       }
     }
--- Packages/Sources/Features/Login/LoginView.swift
+++ Packages/Sources/Features/Login/LoginView.swift
@@ -1,8 +1,15 @@
+import Casimir
 import ComposableArchitecture
 import SwiftUI
 
 public struct LoginView: View {
   private let store: StoreOf<LoginReducer>
+  
+//  struct ViewState: Equatable {
+//    init(state: LoginReducer.State) {
+//
+//    }
+//  }
   
   public init(
     store: StoreOf<LoginReducer>
@@ -11,9 +18,66 @@ public struct LoginView: View {
   }
   
   public var body: some View {
+//    WithViewStore(store, observe: ViewState.init) { viewStore in
     WithViewStore(store) { viewStore in
-      
+      ZStack {
+        gradientBackground()
+          .ignoresSafeArea()
+        
+        overlay(viewStore)
+          .maxHeight(.infinity)
+        
+        Text(viewStore.rawValue)
+      }
     }
+  }
+}
+
+private extension View {
+  @ViewBuilder
+  func gradientBackground() -> some View {
+    LinearGradient(
+      stops: [
+        .init(color: Color.red, location: .zero),
+        .init(color: Color.green, location: 1)
+      ],
+      startPoint: .bottomLeading,
+      endPoint: .topTrailing
+    )
+    .opacity(0.33)
+  }
+  
+  @ViewBuilder
+  func overlay(
+//    _ viewStore: ViewStore<LoginView.ViewState, LoginReducer.Action>
+    _ viewStore: ViewStore<LoginReducer.State, LoginReducer.Action>
+  ) -> some View {
+    VStack {
+      VStack {
+        VStack {}
+        .maxHeight(.infinity)
+        
+        VStack {
+          Text("Welcome to Havana, your new GitHub client.")
+            .font(.title)
+        }
+        .multilineTextAlignment(.center)
+        .maxHeight(.infinity)
+      }
+      .maxHeight(.infinity)
+      
+      VStack {
+        Button("Login with Private Access Token") {
+          viewStore.send(.userDidTapLoginWithPrivateAccessTokenButton)
+        }
+        Button("Login with GitHub.com") {
+          viewStore.send(.userDidTapLoginWithGithubButton)
+        }
+      }
+      .maxHeight(.infinity)
+      .background(.white)
+    }
+    .padding()
   }
 }
 
@@ -21,7 +85,7 @@ struct LoginView_Previews: PreviewProvider {
   static var previews: some View {
     LoginView(
       store: Store(
-        initialState: LoginReducer.State.privateAccessTokenFlow,
+        initialState: LoginReducer.State.greeting,
         reducer: LoginReducer()
       )
     )
