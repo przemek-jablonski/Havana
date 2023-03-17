71da4f7a754591ad10835afda84ab6e0e2f49eaf
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-03-17 18:32:00 +0100

    Moved URLSessionNetworkClient implementation to Casimir

--- Packages/Sources/Core/Octokit/Live/api_2022_11_28/Internals/NetworkClient/URLSessionNetworkClient.swift
+++ Packages/Sources/Core/Octokit/Live/api_2022_11_28/Internals/NetworkClient/URLSessionNetworkClient.swift
@@ -23,6 +23,14 @@ extension URLSessionNetworkClient: NetworkClient {
     _ type: ReturnType.Type,
     using data: NetworkClientRequestData
   ) async -> Result<ReturnType, NetworkClientError> {
+    await urlSessionInstance.perform(
+      requestTo: data.url,
+      using: data.method,
+      headers: data.headers ?? [:],
+      queryItems: data.queryItems ?? [:],
+      encoding: data.body,
+      using: jsonEncoder
+    )
     
 //    request(data)
 //      .flatMap { [deserializer] response in
@@ -31,94 +39,5 @@ extension URLSessionNetworkClient: NetworkClient {
 //          .mapError(NetworkClientError.serverResponseDeserializationFailure)
 //      }
 //      .eraseToAnyPublisher()
-  }
-  
-  private func request(
-    _ data: NetworkClientRequestData
-  ) async -> Result<Data, NetworkClientError> {
-    
-    URLSession.shared
-    
-//    urlSessionInstance.data(for: <#T##URLRequest#>)
-    // TODO: move some of this to Casimir
-    URLRequest
-      .construct(
-        data.url,
-        data.method.rawValue,
-        data.body,
-        data.headers ?? [:],
-        data.queryItems ?? [:],
-        using: jsonEncoder
-      )
-      .mapError(NetworkClientError.serverRequestConstructionFailed)
-      .flatMap { [urlSessionInstance] request in
-        
-        urlSessionInstance
-          .dataTaskPublisher(for: request)
-          .processResponse()
-      }
-      .map { (data: Data, response: URLResponse) in data }
-  }
-}
-
-//private extension URLRequest {
-//  enum URLRequestConstructionError: Error {
-//    case constructingURLComponentsWithQueryItemsFailure(url: String, queryItems: [String: String])
-//    case extracingURLFromURLComponentsFailure(url: String, queryItems: [String: String])
-//    case encodingRequestBodyFailure
-//  }
-//
-//  static func from(
-//    _ url: String,
-//    _ httpMethod: String,
-//    _ body: Encodable?,
-//    _ headers: [String: String],
-//    _ queryItems: [String: String],
-//    using jsonEncoder: JSONEncoder
-//  ) -> Result<Self, URLRequestConstructionError>.Publisher {
-//
-//    guard let urlComponents = URLComponents(
-//      url: url,
-//      queryItems: queryItems.map { URLQueryItem(name: $0.key, value: $0.value) }
-//    ) else {
-//      return .failure(.constructingURLComponentsWithQueryItemsFailure(url: url, queryItems: queryItems))
-//    }
-//
-//    guard let url = urlComponents.url else {
-//      return .failure(.extracingURLFromURLComponentsFailure(url: url, queryItems: queryItems))
-//    }
-//
-//    var request = URLRequest(url: url)
-//    request.httpMethod = httpMethod
-//
-//    headers.forEach {
-//      request.addValue($0.value, forHTTPHeaderField: $0.key)
-//    }
-//
-//    do {
-//      request.httpBody = try body?.data(using: jsonEncoder)
-//    } catch {
-//      return .failure(.encodingRequestBodyFailure)
-//    }
-//
-//    return .success(request)
-//  }
-//}
-
-private extension URLSession.DataTaskPublisher {
-  func processResponse() -> some Publisher<Output, NetworkClientError> {
-    self
-      .mapError(NetworkClientError.internalNetworkClientFailure)
-      .flatMap { (data: Data, response: URLResponse) in
-        guard let response = response as? HTTPURLResponse else {
-          return Fail<Output, NetworkClientError>(error: NetworkClientError.serverReturnedNonHTTPContent).eraseToAnyPublisher()
-        }
-        
-        guard (200..<300) ~= response.statusCode else {
-          return Fail(error: .serverReturnedInvalidStatusCode(code: response.statusCode)).eraseToAnyPublisher()
-        }
-        
-        return Just((data, response)).setFailureType(to: NetworkClientError.self).eraseToAnyPublisher()
-      }
   }
 }
