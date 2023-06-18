cf7cf6031b576ba261d50c727723ee3363af49a9
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-06-18 12:41:56 +0200

    Cleanup

--- Packages/Sources/Core/Octokit/Live/api_2022_11_28/Internals/Misc/EventsMapper/EventsDecoder_API_2022_11_28.swift
+++ Packages/Sources/Core/Octokit/Live/api_2022_11_28/Internals/Misc/EventsMapper/EventsDecoder_API_2022_11_28.swift
@@ -51,6 +51,8 @@ private extension EventsDecoder {
       }
   }
 
+  // swiftlint:disable cyclomatic_complexity
+  // swiftlint:disable function_body_length
   func decode(
     json: [String: Any],
     using jsonDecoder: JSONDecoder
@@ -75,70 +77,72 @@ private extension EventsDecoder {
     }
 
     switch eventType {
-      case Octokit.EventType.commitComment.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.CommitCommentEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .commitCommentEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.create.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.CreateEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .createEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.delete.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.DeleteEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .deleteEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.fork.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.ForkEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .forkEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.gollum.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.GollumEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .gollumEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.issueComment.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.IssueCommentEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .issueCommentEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.issues.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.IssuesEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .issuesEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.member.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.MemberEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .memberEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.public.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.PublicEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .publicEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.pullRequest.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.PullRequestEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .pullRequestEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.pullRequestReview.string:
-        return .failure(.unsupportedEventType(Octokit.EventType.pullRequestReview.string, json))
-      case Octokit.EventType.pullRequestReviewComment.string:
-        return .failure(.unsupportedEventType(Octokit.EventType.pullRequestReviewComment.string, json))
-      case Octokit.EventType.pullRequestReviewThread.string:
-        return .failure(.unsupportedEventType(Octokit.EventType.pullRequestReviewThread.string, json))
-      case Octokit.EventType.push.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.PushEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .pushEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.release.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.ReleaseEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .releaseEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.sponsorship.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.SponsorshipEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .sponsorshipEvent(.init(id: eventId, payload: $0)) }
-      case Octokit.EventType.watch.string:
-        return jsonDecoder.decode(Octokit.Event.Payload.WatchEventPayload.self, using: eventPayloadData)
-          .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
-          .map { .watchEvent(.init(id: eventId, payload: $0)) }
-      default:
-        return .failure(.unknownEventType(json))
+    case Octokit.EventType.commitComment.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.CommitCommentEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .commitCommentEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.create.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.CreateEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .createEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.delete.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.DeleteEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .deleteEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.fork.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.ForkEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .forkEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.gollum.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.GollumEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .gollumEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.issueComment.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.IssueCommentEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .issueCommentEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.issues.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.IssuesEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .issuesEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.member.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.MemberEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .memberEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.public.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.PublicEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .publicEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.pullRequest.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.PullRequestEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .pullRequestEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.pullRequestReview.string:
+      return .failure(.unsupportedEventType(Octokit.EventType.pullRequestReview.string, json))
+    case Octokit.EventType.pullRequestReviewComment.string:
+      return .failure(.unsupportedEventType(Octokit.EventType.pullRequestReviewComment.string, json))
+    case Octokit.EventType.pullRequestReviewThread.string:
+      return .failure(.unsupportedEventType(Octokit.EventType.pullRequestReviewThread.string, json))
+    case Octokit.EventType.push.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.PushEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .pushEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.release.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.ReleaseEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .releaseEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.sponsorship.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.SponsorshipEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .sponsorshipEvent(.init(id: eventId, payload: $0)) }
+    case Octokit.EventType.watch.string:
+      return jsonDecoder.decode(Octokit.Event.Payload.WatchEventPayload.self, using: eventPayloadData)
+        .mapError { .unableToDecodeJsonIntoEventObject(json, $0) }
+        .map { .watchEvent(.init(id: eventId, payload: $0)) }
+    default:
+      return .failure(.unknownEventType(json))
     }
   }
+  // swiftlint:enable cyclomatic_complexity
+  // swiftlint:enable function_body_length
 }
--- Packages/Sources/Core/Octokit/Tests/api_2022_11_28/Internals/Misc/JSONDecoderAPI_2022_11_28TestCase.swift
+++ Packages/Sources/Core/Octokit/Tests/api_2022_11_28/Internals/Misc/JSONDecoderAPI_2022_11_28TestCase.swift
@@ -141,66 +141,6 @@ internal final class JSONDecoderAPI_2022_11_28DecodingTestCase: XCTestCase {
     )
   }
 
-  //  internal func test_decodingExampleEventsFile_doesntThrow_1() async throws {
-  //    try await performDecoding(of: "events_1.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_2() async throws {
-  //    try await performDecoding(of: "events_2.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_3() async throws {
-  //    try await performDecoding(of: "events_3.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_4() async throws {
-  //    try await performDecoding(of: "events_4.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_5() async throws {
-  //    try await performDecoding(of: "events_5.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_6() async throws {
-  //    try await performDecoding(of: "events_6.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_7() async throws {
-  //    try await performDecoding(of: "events_7.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_8() async throws {
-  //    try await performDecoding(of: "events_8.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_9() async throws {
-  //    try await performDecoding(of: "events_9.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_10() async throws {
-  //    try await performDecoding(of: "events_10.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_11() async throws {
-  //    try await performDecoding(of: "events_11.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_12() async throws {
-  //    try await performDecoding(of: "events_12.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_13() async throws {
-  //    try await performDecoding(of: "events_13.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_14() async throws {
-  //    try await performDecoding(of: "events_14.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-  //
-  //  internal func test_decodingExampleEventsFile_doesntThrow_15() async throws {
-  //    try await performDecoding(of: "events_15.json", to: Octokit.Event.self, using: JSONDecoder.forAPI_2022_11_28)
-  //  }
-
   // MARK: - Octokit.Event.self
 
   //  internal func test_decodingExampleEventsFile_doesntThrow_01() throws {
@@ -236,7 +176,7 @@ private extension XCTestCase {
     let decodingResult = await EventsDecoder_API_2022_11_28(
       jsonDecoder: .forAPI_2022_11_28
     )
-      .decodeEvents(from: jsonData)
+    .decodeEvents(from: jsonData)
 
     try assertions?(decodingResult)
   }
