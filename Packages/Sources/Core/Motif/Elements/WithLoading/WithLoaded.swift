5152babe1467c8e8b6f16039f337480cff726fff
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-07-02 14:36:38 +0200

    WithLoaded with single view parent at the root

--- Packages/Sources/Core/Motif/Elements/WithLoading/WithLoaded.swift
+++ Packages/Sources/Core/Motif/Elements/WithLoading/WithLoaded.swift
@@ -2,82 +2,88 @@ import Casimir
 import SwiftUI
 
 /**
- View that morphs it's content depending on the `data` state (switches between displaying "loading" view, "error" view and the actual content).
+ List that morphs it's content depending on the `data` state (switches between displaying "loading" view, "error" view and the actual content).
  */
-public struct WithLoaded<LoadedView: View, Data: Equatable>: View {
-  private var data: Loadable<Data>
-  private let loadingPrompt: String?
-  private let loadedView: (_ loaded: Data) -> LoadedView
+public struct WithLoaded<
+  Model: ModelProtocol,
+  Container: Collection & Equatable & RandomAccessCollection,
+  LoadedView: View
+>: View where Container.Element == Model {
+
+  private let placeholdersCount = 20
+
+  private var loadables: Loadable<Container>
+  private var loadedView: (_ element: Model) -> LoadedView
 
   public init(
-    _ data: Loadable<Data>,
-    loadingPrompt: String? = nil, // TODO: loading prompt should be mandatory to include
-    @ViewBuilder loadedView: @escaping (_ loaded: Data) -> LoadedView
+    _ loadables: Loadable<Container>,
+    @ViewBuilder loadedView: @escaping (_ element: Model) -> LoadedView
   ) {
-    self.data = data
-    self.loadingPrompt = loadingPrompt
+    self.loadables = loadables
     self.loadedView = loadedView
   }
 
   public var body: some View {
-    if case Loadable.loading = data {
-      loadingView()
-    }
-    if case Loadable.failure(let error) = data {
-      errorView(error)
-    }
-    if case Loadable.loaded(let data) = data {
-      loadedView(data)
+    ZStack {
+      List {
+        if loadables.isLoading {
+          ForEach((0..<placeholdersCount).map { _ in Model.random() }) { model in
+            loadedView(model)
+          }
+          .redacted(reason: .placeholder)
+        }
+
+        if case Loadable.loaded(let loaded) = loadables {
+          ForEach(loaded) { element in
+            loadedView(element)
+          }
+        }
+      }
+
+      if case Loadable.failure(let error) = loadables {
+        VStack {
+          Image(systemName: "xmark.circle.fill")
+          Text(error.localizedDescription)
+        }
+        .padding()
+        .padding(.vertical)
+        .foregroundColor(.secondary)
+        .background(.ultraThinMaterial)
+      }
     }
   }
 }
 
-private extension WithLoaded {
-  @ViewBuilder
-  func loadingView() -> some View {
-    ProgressView {
-      if let loadingPrompt {
-        Text(loadingPrompt)
-      }
-    }
-  }
-
-  @ViewBuilder
-  func errorView(_ error: Error) -> some View {
-    Text(error.localizedDescription)
-  }
-}
-
-// TODO: does 'DEBUG' work
-#if DEBUG
-internal struct LoadingView_Previews: PreviewProvider {
-  internal static var previews: some View {
-    Group {
-      WithLoaded(Loadable<String>.loaded(.random())) { loaded in
-        loadedView(loaded)
-      }
-
-      WithLoaded(Loadable<String>.loading) { loaded in
-        loadedView(loaded)
-      }
-
-      WithLoaded(Loadable<String>.loading, loadingPrompt: .random()) { loaded in
-        loadedView(loaded)
-      }
-
-      WithLoaded(Loadable<String>.failure(GenericError(description: .random()))) { loaded in
-        loadedView(loaded)
-      }
-    }
-  }
-
-  @ViewBuilder
-  private static func loadedView(
-    _ string: String
-  ) -> some View {
-    Label(string, systemImage: "paperplane.circle.fill")
-      .padding()
-      .background(Color.random)
-  }
-}
-#endif
+//// TODO: does 'DEBUG' work
+// #if DEBUG
+// internal struct LoadingView_Previews: PreviewProvider {
+//  internal static var previews: some View {
+//    Group {
+//      WithLoaded(Loadable<String>.loaded(.random())) { loaded in
+//        loadedView(loaded)
+//      }
+//
+//      WithLoaded(Loadable<String>.loading) { loaded in
+//        loadedView(loaded)
+//      }
+//
+//      WithLoaded(Loadable<String>.loading, loadingPrompt: .random()) { loaded in
+//        loadedView(loaded)
+//      }
+//
+//      WithLoaded(Loadable<String>.failure(GenericError(description: .random()))) { loaded in
+//        loadedView(loaded)
+//      }
+//    }
+//  }
+//
+//  @ViewBuilder
+//  private static func loadedView(
+//    _ string: String
+//  ) -> some View {
+//    Label(string, systemImage: "paperplane.circle.fill")
+//      .padding()
+//      .background(Color.random)
+//  }
+// }
+// #endif
